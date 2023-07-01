de8eb401d503d05c37f4e9eb6d311036532a0122
Romwel Feliciano <127919521+RomwelFeliciano@users.noreply.github.com>
2023-07-01 19:37:04 +0800

    change image

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import "./App.css";
 
 const App = () => {
   return (
-    <div className="flex justify-center items-center h-screen bg-[url(https://images.unsplash.com/photo-1580674287405-80cd77a2fee2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80)] bg-no-repeat bg-cover bg-center font-league">
+    <div className="flex justify-center items-center h-screen bg-[url(https://images.pexels.com/photos/3987223/pexels-photo-3987223.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)] bg-no-repeat bg-cover bg-center font-league">
       <GroceryComponent />
     </div>
   );
